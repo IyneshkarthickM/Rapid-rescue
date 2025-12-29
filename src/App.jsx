4c2d809be765ddc59502ea5552a33b6e1e1a64cc
IyneshkarthickM <iyneshkarthick67@gmail.com>
2025-12-29 10:50:14 +0530

    Fix component import casing for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import Home from './Pages/Home';
+// import LoginForm from './components/LoginForm';  // Ensure correct filename casing
+// import DriverSignup from './components/DriverSignup';
+// import MapComponent from './components/MapComponent';
+// import Mapforambulance from './components/Mapforambulance';
+// import MedulanceSupport from './components/Medulancesupport';
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/LoginForm" element={<LoginForm />}/>
+//         <Route path="/DriverSignup" element={<DriverSignup/> }/>
+//         <Route path="/MapComponent" element={<MapComponent/> }/>
+//         <Route path="/Mapforambulance" element={<Mapforambulance/>}/>
+//         <Route path="/MedulanceSupport" element={<MedulanceSupport/>}/>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Home from './Pages/Home';
-import LoginForm from './components/LoginForm';  // Ensure correct filename casing
+import LoginForm from './components/LoginForm';
 import DriverSignup from './components/DriverSignup';
 import MapComponent from './components/MapComponent';
 import Mapforambulance from './components/Mapforambulance';
-import MedulanceSupport from './components/medulancesupport';
-
+import MedulanceSupport from './components/MedulanceSupport';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/LoginForm" element={<LoginForm />}/>
-        <Route path="/DriverSignup" element={<DriverSignup/> }/>
-        <Route path="/MapComponent" element={<MapComponent/> }/>
-        <Route path="/Mapforambulance" element={<Mapforambulance/>}/>
-        <Route path="/MedulanceSupport" element={<MedulanceSupport/>}/>
+        <Route path="/LoginForm" element={<LoginForm />} />
+        <Route path="/DriverSignup" element={<DriverSignup />} />
+        <Route path="/MapComponent" element={<MapComponent />} />
+        <Route path="/Mapforambulance" element={<Mapforambulance />} />
+        <Route path="/MedulanceSupport" element={<MedulanceSupport />} />
       </Routes>
     </BrowserRouter>
   );
